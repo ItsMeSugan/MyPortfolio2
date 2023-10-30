343699fd2299561f634ae12bbe04f998b40bb92f
Sugan T <t.suganeeshwaran@gmail.com>
2023-10-30 12:53:24 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,46 @@
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utilis";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className={styles.navbar}>
+      <a href="/" className={styles.title}>
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="MenuIcon"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems}  ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
